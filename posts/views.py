d0a8e2e1d6f04fff339d55bbc5e72f56566b62bb
saikalonie <saikalanarbekovaabakir@gmail.com>
2025-01-19 20:59:52 +0600

    Lesson 4, static files, another table columns

--- posts/views.py
+++ posts/views.py
@@ -12,9 +12,9 @@ def posts_list_view(request):
         posts = Post.objects.all()
     return render(request, "post__list.html", context={"posts": posts})
 
-def posts_detail_view(request, id):
+def posts_detail_view(request, post_id):
      if request.method == 'GET':
-         posts = Post.objects.get(id=id)
+         posts = Post.objects.get(id=post_id)
      return render(request, "post_detail.html", context={"post": posts})
 
 # def post_detail_view(request, pk):
